6c1536043c5bc7436a68b70f3787e6d6bf4e31df
Tarrou501 <tarroung@gmail.com>
2022-01-22 16:21:08 -0300

    String de Conexao, DBContext e configuração Startup para conexão do MySQL

--- WebApp.API/Startup.cs
+++ WebApp.API/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -10,22 +11,31 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApp.Repositorio.Contexto;
 
 namespace WebApp.API
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder();
+            builder.AddJsonFile("config.json", optional: false, reloadOnChange: true);
+            Configuration = builder.Build();
         }
 
-        public IConfiguration Configuration { get; }
+
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            var connectionString = Configuration.GetConnectionString("WebAppDBConnection");
+            services.AddDbContext<WebAppContexto>(options =>
+                                   options.UseMySql(connectionString,
+                                              m => m.MigrationsAssembly("WebApp.Repositorio")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ WebApp.Repositorio/Contexto/WebAppContexto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebApp.Dominio.Entidades;
+
+namespace WebApp.Repositorio.Contexto
+{
+    public class WebAppContexto: DbContext
+    {
+        public DbSet<Cliente> Clientes { get; set; }
+
+
+        public WebAppContexto(DbContextOptions options) : base(options)
+        {
+        }
+
+    }
+}
