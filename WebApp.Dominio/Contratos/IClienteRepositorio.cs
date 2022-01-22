7444192c6d4b1ed0bb308ce622f5f2db76d2c7da
Tarrou501 <tarroung@gmail.com>
2022-01-22 15:00:40 -0300

    Contratos (Inteface Cliente)

---
+++ WebApp.Dominio/Contratos/IClienteRepositorio.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebApp.Dominio.Entidades;
+
+namespace WebApp.Dominio.Contratos
+{
+    public interface IClienteRepositorio:IBaseRepositorio<Cliente> 
+    {
+    }
+}
