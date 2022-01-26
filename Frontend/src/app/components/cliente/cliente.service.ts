b6c29c9a3491b8ddf7f07687ed58541f7b3ffa3a
Tarrou501 <tarroung@gmail.com>
2022-01-26 00:58:16 -0300

    Ajustes Finais

--- Frontend/src/app/components/cliente/cliente.service.ts
+++ Frontend/src/app/components/cliente/cliente.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {MatSnackBar} from '@angular/material/snack-bar';
-import { Observable } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { catchError, EMPTY, map, Observable } from 'rxjs';
 import { Cliente } from './cliente.model';
 
 @Injectable({
@@ -10,39 +10,61 @@ import { Cliente } from './cliente.model';
 export class ClienteService {
   baseUrl = "https://localhost:44362/api/cliente";
 
-  constructor(private snackBar:MatSnackBar, private http: HttpClient) { }
+  constructor(private snackBar: MatSnackBar, private http: HttpClient) { }
 
-  showMessage(msg:string):void{
-    this.snackBar.open(msg,'X',{
-      duration:3000,
-      horizontalPosition:"right",
-      verticalPosition:"top"
+  showMessage(msg: string, isError: boolean = false): void {
+    this.snackBar.open(msg, 'X', {
+      duration: 3000,
+      horizontalPosition: "right",
+      verticalPosition: "top",
+      panelClass: isError ? ['msg-error'] : ['msg-success']
+      // panelClass: 'notif-success'
     });
   }
-  
-  create(cliente: Cliente): Observable<Cliente>{
+
+  create(cliente: Cliente): Observable<Cliente> {
     console.log(cliente);
-     return this.http.post<Cliente>(this.baseUrl,cliente);  
+    return this.http.post<Cliente>(this.baseUrl, cliente).pipe(
+      map(obj => obj),
+      catchError(e => this.ErrorHandlee(e))
+    );
 
   }
 
-  read(): Observable<Cliente[]>{
-    return this.http.get<Cliente[]>(this.baseUrl);   
+  ErrorHandlee(e: any): Observable<any> {
+    this.showMessage('Ocorreu um erro!', true);
+    return EMPTY;
   }
 
-  readById(id:string): Observable<Cliente>{
+  read(): Observable<Cliente[]> {
+    return this.http.get<Cliente[]>(this.baseUrl).pipe(
+      map(obj => obj),
+      catchError(e => this.ErrorHandlee(e))
+    );
+  }
+
+  readById(id: string): Observable<Cliente> {
     const url = `${this.baseUrl}/${id}`;
-    return this.http.get<Cliente>(url);   
+    return this.http.get<Cliente>(url).pipe(
+      map(obj => obj),
+      catchError(e => this.ErrorHandlee(e))
+    );
   }
 
-  update(cliente: Cliente): Observable<Cliente>{
+  update(cliente: Cliente): Observable<Cliente> {
     //const url = `${this.baseUrl}/${cliente.id}`;
 
-    return  this.http.put<Cliente>(this.baseUrl,cliente);   
+    return this.http.put<Cliente>(this.baseUrl, cliente).pipe(
+      map(obj => obj),
+      catchError(e => this.ErrorHandlee(e))
+    );
   }
 
-  delete(id:number): Observable<Cliente>{
+  delete(id: number): Observable<Cliente> {
     const url = `${this.baseUrl}/${id}`;
-    return this.http.delete<Cliente>(url);   
+    return this.http.delete<Cliente>(url).pipe(
+      map(obj => obj),
+      catchError(e => this.ErrorHandlee(e))
+    );
   }
 }
